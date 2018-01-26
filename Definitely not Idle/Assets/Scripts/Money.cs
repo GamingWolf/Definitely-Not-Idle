8a6d1bea57a385e3ddd450479f423849cb3d0cee
Lord Potato Laptop <darealgamingwolf@gmail.com>
2018-01-26 11:09:43 +0100

    123

---
+++ Definitely not Idle/Assets/Scripts/Money.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Money : MonoBehaviour {
+
+	private string ducatString;
+	private int bSize = 1000 ;
+	private int aSize = 1 ;
+
+
+	string MoneyScale(double number)
+	{
+		double aScore = Math.Floor (number / aSize);
+		double bScore = Math.Floor ((number - aScore * aSize) / bSize);
+
+		return ducatString;
+	}
+
+	private void SuffixScale(string s)
+	{
+		
+	}
+}
